0f488c2cf74657421b1fc225e1f79a2a9509b5cb
Alexandre MERLE <amerle@e3r2p12.42.fr>
2013-08-14 15:29:51 +0200

    Check space and tab ok

--- test_space.sh
+++ test_space.sh
@@ -1,1 +1,7 @@
-cat $1 | grep -E -n "[ \t]$"
+#!/bin/sh
+
+echo '';
+cat $1 | grep -E -n ' $' | xargs echo;
+echo "## Espace en fin de ligne ##\n";
+cat $1 | grep -E -n '	$' | xargs echo;
+echo "## Tab en fin de ligne ##\n";
